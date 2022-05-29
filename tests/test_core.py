ac0a615c8cde2ed682baf3b98cad96c88424b919
estripling <estripling042@gmail.com>
2022-05-29 17:43:24 +0200

    feat(core.py): add remove_punctuation

---
+++ tests/test_core.py
@@ -0,0 +1,45 @@
+import pytest
+
+from bumbag import core
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("no punctuation", "no punctuation"),
+        (" text with whitespaces ", " text with whitespaces "),
+        ("CAPITAL LETTERS", "CAPITAL LETTERS"),
+        ("exclamation mark!", "exclamation mark"),
+        ('quotation mark"', "quotation mark"),
+        ("hash#", "hash"),
+        ("dollar$", "dollar"),
+        ("percentage%", "percentage"),
+        ("ampersand&", "ampersand"),
+        ("apostrophe'", "apostrophe"),
+        ("asterix*", "asterix"),
+        ("plus+", "plus"),
+        ("comma,", "comma"),
+        ("dash-", "dash"),
+        ("period.", "period"),
+        ("slash/", "slash"),
+        ("colon:", "colon"),
+        ("semicolon;", "semicolon"),
+        ("less than sign<", "less than sign"),
+        ("equal sign=", "equal sign"),
+        ("greater than sign>", "greater than sign"),
+        ("question mark?", "question mark"),
+        ("at sign@", "at sign"),
+        ("backslash\\", "backslash"),
+        ("caret^", "caret"),
+        ("underscore_", "underscore"),
+        ("backtick`", "backtick"),
+        ("vertical bar symbol|", "vertical bar symbol"),
+        ("tilde~", "tilde"),
+        ("(round brackets)", "round brackets"),
+        ("{curly brackets}", "curly brackets"),
+        ("[square brackets]", "square brackets"),
+    ],
+)
+def test_remove_punctuation(text: str, expected: str):
+    output = core.remove_punctuation(text)
+    assert output == expected
