76dbb210e7d29a8d57e53a434d9859574df4929c
estripling <estripling042@gmail.com>
2022-06-04 14:07:53 +0200

    feat(math.py): add iseven

--- tests/test_math.py
+++ tests/test_math.py
@@ -12,3 +12,10 @@ def test_iseq(start, n):
     r = range(start, k + 1) if start or start == n else range(start, k)
     expected = tuple(r)[:n]
     assert actual == expected
+
+
+@pytest.mark.parametrize("number", [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+def test_iseven(number):
+    actual = math.iseven(number)
+    expected = number % 2 == 0
+    assert actual == expected
