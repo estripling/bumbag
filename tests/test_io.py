2a4389f566d02b8576736704ae32b5fa43f34b47
estripling <estripling042@gmail.com>
2022-07-06 23:43:51 +0200

    feat(io.py): add archive_files

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,4 +1,7 @@
+import os
 from io import StringIO
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import pytest
 
@@ -35,3 +38,26 @@ class TestQueryYesNo:
         monkeypatch.setattr("sys.stdin", StringIO("yay"))
         with pytest.raises(EOFError):
             io.query_yes_no("Do you like BumBag?", "yes")
+
+
+@pytest.mark.parametrize("kind", ["zip", "gztar"])
+def test_archive_files(kind):
+    with TemporaryDirectory() as tmpdir:
+        path = Path(tmpdir).joinpath("test_file.txt")
+
+        with path.open("w") as fh:
+            fh.write("Hello, World!\n")
+
+        dir_path = Path(tmpdir).joinpath("test_directory/")
+        os.makedirs(dir_path)
+        path2 = Path(dir_path).joinpath("test_file2.txt")
+
+        with path2.open("w") as fh:
+            fh.write("Hello, Again!\n")
+
+        io.archive_files(target_dir=tmpdir, name="archive", kind=kind)
+
+    if kind == "gztar":
+        kind = "tar.gz"
+
+    os.remove(f"archive.{kind}")
