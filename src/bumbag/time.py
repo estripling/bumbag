497afb8025c9b905349a77cab410e53d490d6210
estripling <estripling042@gmail.com>
2022-08-06 00:12:38 +0200

    style(time.py): sort functions alphabetically

--- src/bumbag/time.py
+++ src/bumbag/time.py
@@ -11,111 +11,51 @@ from bumbag.core import op
 from bumbag.math import irange
 
 
-def to_date(string_to_cast):
-    """Cast an ISO date string to a date object.
+def datedelta(reference, days):
+    """Compute date relative to reference date.
+
+    The reference date and relative date are the inclusive endpoints of the
+    corresponding, consecutive date sequence. As a result, the reference date
+    and relative date can, for example, directly be used in a BETWEEN statement
+    of a SQL query.
 
     Parameters
     ----------
-    string_to_cast : str
-        Date string in ISO format (YYYY-MM-DD) to cast.
+    reference : datetime.date
+        The reference date.
+    days : int
+        Size of the delta expressed in number of days:
+         - If ``days == 0``, returns the reference date.
+         - If ``days > 0``, returns the date ahead w.r.t. the reference date.
+         - If ``days < 0``, returns the date ago w.r.t. the reference date.
+        The value of ``days`` equals the length of the corrsponding sequence of
+        consecutive dates with inclusive endpoints.
 
     Returns
     -------
     datetime.date
-        Date object.
+        Relative date.
 
     Examples
     --------
-    >>> to_date("2022-01-01")
+    >>> from datetime import date
+    >>> datedelta(date(2022, 1, 1), 0)
     datetime.date(2022, 1, 1)
+
+    >>> datedelta(date(2022, 1, 1), 3)
+    datetime.date(2022, 1, 3)
+
+    >>> datedelta(date(2022, 1, 1), -3)
+    datetime.date(2021, 12, 30)
     """
-    return datetime.strptime(string_to_cast, "%Y-%m-%d").date()
-
-
-def to_str(date_to_cast):
-    """Cast a date object to an ISO date string.
-
-    Parameters
-    ----------
-    date_to_cast : datetime.date
-        Date object to cast.
-
-    Returns
-    -------
-    str
-        Date string in ISO format (YYYY-MM-DD).
-
-    Examples
-    --------
-    >>> from datetime import date
-    >>> to_str(date(2022, 1, 1))
-    '2022-01-01'
-    """
-    return date_to_cast.isoformat()
-
-
-def last_date_of_month(year, month):
-    """Get last date of month.
-
-    Parameters
-    ----------
-    year : int
-        Year of date.
-    month : int
-        Month of date.
-
-    Returns
-    -------
-    datetime.date
-        Last date of month.
-
-    Examples
-    --------
-    >>> last_date_of_month(2022, 1)
-    datetime.date(2022, 1, 31)
-    """
-    _, number_days_in_month = calendar.monthrange(year, month)
-    return date(year, month, number_days_in_month)
-
-
-def days_between_dates(date1, date2, include_last_date=False):
-    """Compute the number of days between two dates.
-
-    Parameters
-    ----------
-    date1 : datetime.date
-        First reference date.
-    date2 : datetime.date
-        Second reference date.
-    include_last_date : bool, default=False
-        Specify if the larger date should be included in the computation:
-         - If ``False``, number of days based on date interval [date1, date2).
-         - If ``True``, number of days based on date interval [date1, date2].
-
-    Notes
-    -----
-    - If ``date1 > date2``, swapping values.
-
-    Returns
-    -------
-    int
-        Number of days.
-
-    Examples
-    --------
-    >>> from datetime import date
-    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 1))
-    0
-    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 1), True)
-    1
-
-    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 7))
-    6
-    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 7), True)
-    7
-    """
-    start, end = sorted([date1, date2])
-    return (end - start).days + 1 if include_last_date else (end - start).days
+    relative_date = reference + timedelta(days=days)
+    return (
+        relative_date
+        if days == 0
+        else relative_date - timedelta(days=1)
+        if days > 0
+        else relative_date + timedelta(days=1)
+    )
 
 
 def daterange(start, end, include_start=True, include_end=True):
@@ -177,6 +117,46 @@ def daterange(start, end, include_start=True, include_end=True):
     return itertools.takewhile(lambda d: d <= end, drange(start))
 
 
+def days_between_dates(date1, date2, include_last_date=False):
+    """Compute the number of days between two dates.
+
+    Parameters
+    ----------
+    date1 : datetime.date
+        First reference date.
+    date2 : datetime.date
+        Second reference date.
+    include_last_date : bool, default=False
+        Specify if the larger date should be included in the computation:
+         - If ``False``, number of days based on date interval [date1, date2).
+         - If ``True``, number of days based on date interval [date1, date2].
+
+    Notes
+    -----
+    - If ``date1 > date2``, swapping values.
+
+    Returns
+    -------
+    int
+        Number of days.
+
+    Examples
+    --------
+    >>> from datetime import date
+    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 1))
+    0
+    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 1), True)
+    1
+
+    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 7))
+    6
+    >>> days_between_dates(date(2022, 8, 1), date(2022, 8, 7), True)
+    7
+    """
+    start, end = sorted([date1, date2])
+    return (end - start).days + 1 if include_last_date else (end - start).days
+
+
 def drange(start, forward=True):
     """Generate an 'infinite' sequence of consecutive dates.
 
@@ -213,93 +193,100 @@ def drange(start, forward=True):
     return toolz.iterate(successor, start)
 
 
-def datedelta(reference, days):
-    """Compute date relative to reference date.
-
-    The reference date and relative date are the inclusive endpoints of the
-    corresponding, consecutive date sequence. As a result, the reference date
-    and relative date can, for example, directly be used in a BETWEEN statement
-    of a SQL query.
+def humantime(seconds):
+    """Convert seconds to human-readable time.
 
     Parameters
     ----------
-    reference : datetime.date
-        The reference date.
-    days : int
-        Size of the delta expressed in number of days:
-         - If ``days == 0``, returns the reference date.
-         - If ``days > 0``, returns the date ahead w.r.t. the reference date.
-         - If ``days < 0``, returns the date ago w.r.t. the reference date.
-        The value of ``days`` equals the length of the corrsponding sequence of
-        consecutive dates with inclusive endpoints.
+    seconds : int, float
+        Seconds to convert, a non-negative number.
+
+    Returns
+    -------
+    str
+        Human-readable time.
+
+    Raises
+    ------
+    ValueError
+        If ``seconds`` is a negative number.
+
+    Examples
+    --------
+    >>> humantime(1)
+    '1 second'
+
+    >>> humantime(2)
+    '2 seconds'
+
+    >>> humantime(60)
+    '1 minute'
+
+    >>> humantime(120)
+    '2 minutes'
+
+    >>> humantime(60 * 60 * 24 + 123456)
+    '2 days, 10 hours, 17 minutes'
+    """
+    if seconds < 0:
+        raise ValueError(f"{seconds=} - must be a non-negative number")
+
+    if isclose(seconds, 0):
+        return "0 seconds"
+
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+
+    def multiplier(time_with_unit: str) -> str:
+        time_without_unit = float(time_with_unit.split(" ")[0])
+        return (
+            time_with_unit
+            if isclose(time_without_unit, 1)
+            else f"{time_with_unit}s"
+        )
+
+    result = []
+    if days:
+        result.append(multiplier(f"{int(days)} day"))
+
+    if hours:
+        result.append(multiplier(f"{int(hours)} hour"))
+
+    if minutes:
+        result.append(multiplier(f"{int(minutes)} minute"))
+
+    if seconds and minutes < 2:
+        if isclose(seconds, int(seconds)):
+            result.append(multiplier(f"{int(seconds)} second"))
+        else:
+            result.append(f"{seconds:0.6f} seconds")
+
+    return ", ".join(result)
+
+
+def last_date_of_month(year, month):
+    """Get last date of month.
+
+    Parameters
+    ----------
+    year : int
+        Year of date.
+    month : int
+        Month of date.
 
     Returns
     -------
     datetime.date
-        Relative date.
+        Last date of month.
 
     Examples
     --------
-    >>> from datetime import date
-    >>> datedelta(date(2022, 1, 1), 0)
-    datetime.date(2022, 1, 1)
-
-    >>> datedelta(date(2022, 1, 1), 3)
-    datetime.date(2022, 1, 3)
-
-    >>> datedelta(date(2022, 1, 1), -3)
-    datetime.date(2021, 12, 30)
+    >>> last_date_of_month(2022, 1)
+    datetime.date(2022, 1, 31)
     """
-    relative_date = reference + timedelta(days=days)
-    return (
-        relative_date
-        if days == 0
-        else relative_date - timedelta(days=1)
-        if days > 0
-        else relative_date + timedelta(days=1)
-    )
-
-
-def months_between_dates(date1, date2, include_last_date=False):
-    """Compute the number of months between two dates.
-
-    Parameters
-    ----------
-    date1 : datetime.date
-        First reference date.
-    date2 : datetime.date
-        Second reference date.
-    include_last_date : bool, default=False
-        Specify if the larger date should be included in the computation:
-         - If ``False``, number of days based on date interval [date1, date2).
-         - If ``True``, number of days based on date interval [date1, date2].
-
-    Notes
-    -----
-    - If ``date1 > date2``, swapping values.
-
-    Returns
-    -------
-    int
-        Number of months.
-
-    Examples
-    --------
-    >>> from datetime import date
-    >>> months_between_dates(date(2022, 1, 1), date(2022, 1, 1))
-    0
-    >>> months_between_dates(date(2022, 1, 1), date(2022, 1, 1), True)
-    1
-
-    >>> months_between_dates(date(2022, 1, 1), date(2022, 8, 31))
-    7
-    >>> months_between_dates(date(2022, 1, 1), date(2022, 8, 1), True)
-    8
-    """
-    start, end = sorted([date1, date2])
-    difference = relativedelta(end, start)
-    n_months = difference.months + 12 * difference.years
-    return n_months + 1 if include_last_date else n_months
+    _, number_days_in_month = calendar.monthrange(year, month)
+    return date(year, month, number_days_in_month)
 
 
 def monthrange(start, end, include_start=True, include_end=True):
@@ -373,6 +360,48 @@ def monthrange(start, end, include_start=True, include_end=True):
     return month_sequence
 
 
+def months_between_dates(date1, date2, include_last_date=False):
+    """Compute the number of months between two dates.
+
+    Parameters
+    ----------
+    date1 : datetime.date
+        First reference date.
+    date2 : datetime.date
+        Second reference date.
+    include_last_date : bool, default=False
+        Specify if the larger date should be included in the computation:
+         - If ``False``, number of days based on date interval [date1, date2).
+         - If ``True``, number of days based on date interval [date1, date2].
+
+    Notes
+    -----
+    - If ``date1 > date2``, swapping values.
+
+    Returns
+    -------
+    int
+        Number of months.
+
+    Examples
+    --------
+    >>> from datetime import date
+    >>> months_between_dates(date(2022, 1, 1), date(2022, 1, 1))
+    0
+    >>> months_between_dates(date(2022, 1, 1), date(2022, 1, 1), True)
+    1
+
+    >>> months_between_dates(date(2022, 1, 1), date(2022, 8, 31))
+    7
+    >>> months_between_dates(date(2022, 1, 1), date(2022, 8, 1), True)
+    8
+    """
+    start, end = sorted([date1, date2])
+    difference = relativedelta(end, start)
+    n_months = difference.months + 12 * difference.years
+    return n_months + 1 if include_last_date else n_months
+
+
 def mrange(start, forward=True):
     """Generate an 'infinite' sequence of consecutive months.
 
@@ -418,73 +447,44 @@ def mrange(start, forward=True):
         )
 
 
-def humantime(seconds):
-    """Convert seconds to human-readable time.
+def to_date(string_to_cast):
+    """Cast an ISO date string to a date object.
 
     Parameters
     ----------
-    seconds : int, float
-        Seconds to convert, a non-negative number.
+    string_to_cast : str
+        Date string in ISO format (YYYY-MM-DD) to cast.
+
+    Returns
+    -------
+    datetime.date
+        Date object.
+
+    Examples
+    --------
+    >>> to_date("2022-01-01")
+    datetime.date(2022, 1, 1)
+    """
+    return datetime.strptime(string_to_cast, "%Y-%m-%d").date()
+
+
+def to_str(date_to_cast):
+    """Cast a date object to an ISO date string.
+
+    Parameters
+    ----------
+    date_to_cast : datetime.date
+        Date object to cast.
 
     Returns
     -------
     str
-        Human-readable time.
-
-    Raises
-    ------
-    ValueError
-        If ``seconds`` is a negative number.
+        Date string in ISO format (YYYY-MM-DD).
 
     Examples
     --------
-    >>> humantime(1)
-    '1 second'
-
-    >>> humantime(2)
-    '2 seconds'
-
-    >>> humantime(60)
-    '1 minute'
-
-    >>> humantime(120)
-    '2 minutes'
-
-    >>> humantime(60 * 60 * 24 + 123456)
-    '2 days, 10 hours, 17 minutes'
+    >>> from datetime import date
+    >>> to_str(date(2022, 1, 1))
+    '2022-01-01'
     """
-    if seconds < 0:
-        raise ValueError(f"{seconds=} - must be a non-negative number")
-
-    if isclose(seconds, 0):
-        return "0 seconds"
-
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-
-    def multiplier(time_with_unit: str) -> str:
-        time_without_unit = float(time_with_unit.split(" ")[0])
-        return (
-            time_with_unit
-            if isclose(time_without_unit, 1)
-            else f"{time_with_unit}s"
-        )
-
-    result = []
-    if days:
-        result.append(multiplier(f"{int(days)} day"))
-
-    if hours:
-        result.append(multiplier(f"{int(hours)} hour"))
-
-    if minutes:
-        result.append(multiplier(f"{int(minutes)} minute"))
-
-    if seconds and minutes < 2:
-        if isclose(seconds, int(seconds)):
-            result.append(multiplier(f"{int(seconds)} second"))
-        else:
-            result.append(f"{seconds:0.6f} seconds")
-
-    return ", ".join(result)
+    return date_to_cast.isoformat()
--- tests/test_time.py
+++ tests/test_time.py
@@ -8,62 +8,6 @@ from bumbag import time
 
 
 @pytest.mark.parametrize(
-    "arg, expected",
-    [
-        ("2022-01-01", date(2022, 1, 1)),
-        ("2022-01-31", date(2022, 1, 31)),
-    ],
-)
-def test_to_date(arg, expected):
-    actual = time.to_date(arg)
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "arg, expected",
-    [
-        (date(2022, 1, 1), "2022-01-01"),
-        (date(2022, 1, 31), "2022-01-31"),
-    ],
-)
-def test_to_str(arg, expected):
-    actual = time.to_str(arg)
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "arg, expected",
-    [
-        (date(2022, 1, 1), date(2022, 1, 31)),
-        (date(2022, 2, 1), date(2022, 2, 28)),
-        (date(2022, 3, 1), date(2022, 3, 31)),
-        (date(2022, 4, 1), date(2022, 4, 30)),
-        (date(2022, 5, 1), date(2022, 5, 31)),
-        (date(2022, 6, 1), date(2022, 6, 30)),
-        (date(2022, 7, 1), date(2022, 7, 31)),
-        (date(2022, 8, 1), date(2022, 8, 31)),
-        (date(2022, 9, 1), date(2022, 9, 30)),
-        (date(2022, 10, 1), date(2022, 10, 31)),
-        (date(2022, 11, 1), date(2022, 11, 30)),
-        (date(2022, 12, 1), date(2022, 12, 31)),
-        (date(1970, 1, 1), date(1970, 1, 31)),
-        (date(1970, 1, 15), date(1970, 1, 31)),
-        (date(1970, 1, 31), date(1970, 1, 31)),
-        (date(2020, 2, 2), date(2020, 2, 29)),
-        (date(2022, 2, 3), date(2022, 2, 28)),
-        (date(2000, 2, 4), date(2000, 2, 29)),
-        (date(1900, 2, 5), date(1900, 2, 28)),
-        (date(2012, 2, 27), date(2012, 2, 29)),
-        (date(2012, 2, 28), date(2012, 2, 29)),
-        (date(2012, 2, 29), date(2012, 2, 29)),
-    ],
-)
-def test_last_date_of_month(arg, expected):
-    actual = time.last_date_of_month(arg.year, arg.month)
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
     "args, expected",
     [
         ((date(2022, 8, 1), date(2022, 8, 1), False), 0),
@@ -164,56 +108,61 @@ def test_datedelta(args, expected):
 
 
 @pytest.mark.parametrize(
-    "args, expected",
+    "arg, expected",
     [
-        ((date(2022, 8, 1), date(2022, 8, 1), False), 0),
-        ((date(2022, 8, 1), date(2022, 8, 1), True), 1),
-        ((date(2022, 8, 1), date(2022, 8, 7), False), 0),
-        ((date(2022, 8, 7), date(2022, 8, 1), False), 0),
-        ((date(2022, 8, 1), date(2022, 8, 7), True), 1),
-        ((date(2022, 8, 7), date(2022, 8, 1), True), 1),
-        ((date(2022, 1, 1), date(2022, 8, 1), False), 7),
-        ((date(2022, 1, 1), date(2022, 8, 1), True), 8),
-        ((date(2022, 1, 1), date(2022, 8, 31), False), 7),
-        ((date(2022, 1, 1), date(2022, 8, 31), True), 8),
-        ((date(2022, 1, 31), date(2022, 8, 31), False), 7),
-        ((date(2022, 1, 31), date(2022, 8, 31), True), 8),
-        ((date(2022, 8, 31), date(2022, 1, 31), False), 7),
-        ((date(2022, 8, 31), date(2022, 1, 31), True), 8),
-        ((date(2022, 1, 15), date(2022, 8, 15), False), 7),
-        ((date(2022, 1, 15), date(2022, 8, 15), True), 8),
-        ((date(2022, 8, 15), date(2022, 1, 15), False), 7),
-        ((date(2022, 8, 15), date(2022, 1, 15), True), 8),
-        ((date(2022, 1, 1), date(2022, 1, 31), False), 0),
-        ((date(2022, 1, 1), date(2022, 2, 1), False), 1),
-        ((date(2020, 2, 1), date(2020, 2, 28), False), 0),
-        ((date(2020, 2, 1), date(2020, 2, 29), False), 0),
-        ((date(2020, 2, 1), date(2020, 3, 1), False), 1),
-        ((date(2020, 2, 29), date(2020, 3, 1), False), 0),
-        ((date(2020, 2, 28), date(2020, 3, 1), False), 0),
-        ((date(2020, 2, 15), date(2020, 3, 1), False), 0),
-        ((date(2020, 2, 2), date(2020, 3, 1), False), 0),
-        ((date(2020, 2, 2), date(2020, 3, 2), False), 1),
-        ((date(2019, 12, 1), date(2020, 2, 1), False), 2),
-        ((date(2019, 12, 2), date(2020, 2, 1), False), 1),
-        ((date(2019, 12, 31), date(2020, 2, 1), False), 1),
-        ((date(2020, 1, 1), date(2020, 2, 1), False), 1),
-        ((date(2020, 1, 2), date(2020, 2, 1), False), 0),
-        ((date(2020, 3, 1), date(2020, 4, 1), False), 1),
-        ((date(2020, 3, 2), date(2020, 4, 1), False), 0),
-        ((date(2020, 4, 1), date(2020, 5, 1), False), 1),
-        ((date(2020, 4, 2), date(2020, 5, 1), False), 0),
-        ((date(2020, 4, 15), date(2020, 5, 1), False), 0),
-        ((date(2020, 4, 15), date(2020, 5, 14), False), 0),
-        ((date(2020, 4, 15), date(2020, 5, 15), False), 1),
-        ((date(2020, 4, 15), date(2020, 5, 16), False), 1),
-        ((date(2020, 1, 1), date(2022, 1, 1), False), 24),
-        ((date(2020, 1, 1), date(2022, 2, 1), False), 25),
+        (-2.0, None),
+        (-1, None),
+        (0, "0 seconds"),
+        (1, "1 second"),
+        (59, "59 seconds"),
+        (60, "1 minute"),
+        (61, "1 minute, 1 second"),
+        (120, "2 minutes"),
+        (60 * 60, "1 hour"),
+        (60 * 60 * 24, "1 day"),
+        (110.0, "1 minute, 50 seconds"),
+        (1.4142135623730951, "1.414214 seconds"),
+        (60 * 60 * 24 + 123456, "2 days, 10 hours, 17 minutes"),
     ],
 )
-def test_months_between_dates(args, expected):
-    date1, date2, include_last_date = args
-    actual = time.months_between_dates(date1, date2, include_last_date)
+def test_humantime(arg, expected):
+    if arg < 0:
+        with pytest.raises(ValueError):
+            time.humantime(arg)
+    else:
+        actual = time.humantime(arg)
+        assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        (date(2022, 1, 1), date(2022, 1, 31)),
+        (date(2022, 2, 1), date(2022, 2, 28)),
+        (date(2022, 3, 1), date(2022, 3, 31)),
+        (date(2022, 4, 1), date(2022, 4, 30)),
+        (date(2022, 5, 1), date(2022, 5, 31)),
+        (date(2022, 6, 1), date(2022, 6, 30)),
+        (date(2022, 7, 1), date(2022, 7, 31)),
+        (date(2022, 8, 1), date(2022, 8, 31)),
+        (date(2022, 9, 1), date(2022, 9, 30)),
+        (date(2022, 10, 1), date(2022, 10, 31)),
+        (date(2022, 11, 1), date(2022, 11, 30)),
+        (date(2022, 12, 1), date(2022, 12, 31)),
+        (date(1970, 1, 1), date(1970, 1, 31)),
+        (date(1970, 1, 15), date(1970, 1, 31)),
+        (date(1970, 1, 31), date(1970, 1, 31)),
+        (date(2020, 2, 2), date(2020, 2, 29)),
+        (date(2022, 2, 3), date(2022, 2, 28)),
+        (date(2000, 2, 4), date(2000, 2, 29)),
+        (date(1900, 2, 5), date(1900, 2, 28)),
+        (date(2012, 2, 27), date(2012, 2, 29)),
+        (date(2012, 2, 28), date(2012, 2, 29)),
+        (date(2012, 2, 29), date(2012, 2, 29)),
+    ],
+)
+def test_last_date_of_month(arg, expected):
+    actual = time.last_date_of_month(arg.year, arg.month)
     assert actual == expected
 
 
@@ -309,6 +258,60 @@ def test_monthrange(args, expected):
     assert actual == expected[1:-1], "excluding start and end dates fails"
 
 
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        ((date(2022, 8, 1), date(2022, 8, 1), False), 0),
+        ((date(2022, 8, 1), date(2022, 8, 1), True), 1),
+        ((date(2022, 8, 1), date(2022, 8, 7), False), 0),
+        ((date(2022, 8, 7), date(2022, 8, 1), False), 0),
+        ((date(2022, 8, 1), date(2022, 8, 7), True), 1),
+        ((date(2022, 8, 7), date(2022, 8, 1), True), 1),
+        ((date(2022, 1, 1), date(2022, 8, 1), False), 7),
+        ((date(2022, 1, 1), date(2022, 8, 1), True), 8),
+        ((date(2022, 1, 1), date(2022, 8, 31), False), 7),
+        ((date(2022, 1, 1), date(2022, 8, 31), True), 8),
+        ((date(2022, 1, 31), date(2022, 8, 31), False), 7),
+        ((date(2022, 1, 31), date(2022, 8, 31), True), 8),
+        ((date(2022, 8, 31), date(2022, 1, 31), False), 7),
+        ((date(2022, 8, 31), date(2022, 1, 31), True), 8),
+        ((date(2022, 1, 15), date(2022, 8, 15), False), 7),
+        ((date(2022, 1, 15), date(2022, 8, 15), True), 8),
+        ((date(2022, 8, 15), date(2022, 1, 15), False), 7),
+        ((date(2022, 8, 15), date(2022, 1, 15), True), 8),
+        ((date(2022, 1, 1), date(2022, 1, 31), False), 0),
+        ((date(2022, 1, 1), date(2022, 2, 1), False), 1),
+        ((date(2020, 2, 1), date(2020, 2, 28), False), 0),
+        ((date(2020, 2, 1), date(2020, 2, 29), False), 0),
+        ((date(2020, 2, 1), date(2020, 3, 1), False), 1),
+        ((date(2020, 2, 29), date(2020, 3, 1), False), 0),
+        ((date(2020, 2, 28), date(2020, 3, 1), False), 0),
+        ((date(2020, 2, 15), date(2020, 3, 1), False), 0),
+        ((date(2020, 2, 2), date(2020, 3, 1), False), 0),
+        ((date(2020, 2, 2), date(2020, 3, 2), False), 1),
+        ((date(2019, 12, 1), date(2020, 2, 1), False), 2),
+        ((date(2019, 12, 2), date(2020, 2, 1), False), 1),
+        ((date(2019, 12, 31), date(2020, 2, 1), False), 1),
+        ((date(2020, 1, 1), date(2020, 2, 1), False), 1),
+        ((date(2020, 1, 2), date(2020, 2, 1), False), 0),
+        ((date(2020, 3, 1), date(2020, 4, 1), False), 1),
+        ((date(2020, 3, 2), date(2020, 4, 1), False), 0),
+        ((date(2020, 4, 1), date(2020, 5, 1), False), 1),
+        ((date(2020, 4, 2), date(2020, 5, 1), False), 0),
+        ((date(2020, 4, 15), date(2020, 5, 1), False), 0),
+        ((date(2020, 4, 15), date(2020, 5, 14), False), 0),
+        ((date(2020, 4, 15), date(2020, 5, 15), False), 1),
+        ((date(2020, 4, 15), date(2020, 5, 16), False), 1),
+        ((date(2020, 1, 1), date(2022, 1, 1), False), 24),
+        ((date(2020, 1, 1), date(2022, 2, 1), False), 25),
+    ],
+)
+def test_months_between_dates(args, expected):
+    date1, date2, include_last_date = args
+    actual = time.months_between_dates(date1, date2, include_last_date)
+    assert actual == expected
+
+
 def test_mrange():
     d1 = date(2022, 1, 1)
 
@@ -346,25 +349,22 @@ def test_mrange():
 @pytest.mark.parametrize(
     "arg, expected",
     [
-        (-2.0, None),
-        (-1, None),
-        (0, "0 seconds"),
-        (1, "1 second"),
-        (59, "59 seconds"),
-        (60, "1 minute"),
-        (61, "1 minute, 1 second"),
-        (120, "2 minutes"),
-        (60 * 60, "1 hour"),
-        (60 * 60 * 24, "1 day"),
-        (110.0, "1 minute, 50 seconds"),
-        (1.4142135623730951, "1.414214 seconds"),
-        (60 * 60 * 24 + 123456, "2 days, 10 hours, 17 minutes"),
+        ("2022-01-01", date(2022, 1, 1)),
+        ("2022-01-31", date(2022, 1, 31)),
     ],
 )
-def test_humantime(arg, expected):
-    if arg < 0:
-        with pytest.raises(ValueError):
-            time.humantime(arg)
-    else:
-        actual = time.humantime(arg)
-        assert actual == expected
+def test_to_date(arg, expected):
+    actual = time.to_date(arg)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        (date(2022, 1, 1), "2022-01-01"),
+        (date(2022, 1, 31), "2022-01-31"),
+    ],
+)
+def test_to_str(arg, expected):
+    actual = time.to_str(arg)
+    assert actual == expected
