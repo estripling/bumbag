1eac0153e1740825baea647c527b2d5c2fecea20
estripling <estripling042@gmail.com>
2022-08-06 00:01:00 +0200

    style(math.py): sort functions alphabetically

--- tests/test_math.py
+++ tests/test_math.py
@@ -4,6 +4,35 @@ import toolz
 from bumbag import math
 
 
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        (-2, None),
+        (-1, None),
+        (0, None),
+        (1, (1,)),
+        (2, (2, 1)),
+        (4, (4, 2, 1)),
+        (7, (7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1)),
+        (11, (11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1)),
+        (12, (12, 6, 3, 10, 5, 16, 8, 4, 2, 1)),
+    ],
+)
+def test_collatz(arg, expected):
+    if arg < 1:
+        with pytest.raises(ValueError):
+            tuple(math.collatz(arg))
+    else:
+        actual = tuple(math.collatz(arg))
+        assert actual == expected
+
+
+def test_fibonacci():
+    actual = tuple(toolz.take(16, math.fibonacci()))
+    expected = (0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610)
+    assert actual == expected
+
+
 @pytest.mark.parametrize("n", [1, 2, 3, 10, 100])
 @pytest.mark.parametrize("start", ["invalid_type", -2, -1, 0, 1, 2])
 @pytest.mark.parametrize("step", ["invalid_type", -1, -0.5, 0, 1, 2])
@@ -36,32 +65,3 @@ def test_isodd(arg):
     is_even_number = arg % 2 == 0
     expected = not is_even_number
     assert actual == expected
-
-
-def test_fibonacci():
-    actual = tuple(toolz.take(16, math.fibonacci()))
-    expected = (0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610)
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    "arg, expected",
-    [
-        (-2, None),
-        (-1, None),
-        (0, None),
-        (1, (1,)),
-        (2, (2, 1)),
-        (4, (4, 2, 1)),
-        (7, (7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1)),
-        (11, (11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1)),
-        (12, (12, 6, 3, 10, 5, 16, 8, 4, 2, 1)),
-    ],
-)
-def test_collatz(arg, expected):
-    if arg < 1:
-        with pytest.raises(ValueError):
-            tuple(math.collatz(arg))
-    else:
-        actual = tuple(math.collatz(arg))
-        assert actual == expected
