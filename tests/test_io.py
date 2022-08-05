3f95b6f9c102e119c9e1ea8cfba6cedf864679b6
estripling <estripling042@gmail.com>
2022-08-05 23:57:33 +0200

    style(io.py): sort functions alphabetically

--- tests/test_io.py
+++ tests/test_io.py
@@ -8,6 +8,29 @@ import pytest
 from bumbag import io
 
 
+@pytest.mark.parametrize("kind", ["zip", "gztar"])
+def test_archive_files(kind):
+    with TemporaryDirectory() as tmpdir:
+        path = Path(tmpdir).joinpath("test_file.txt")
+
+        with path.open("w") as fh:
+            fh.write("Hello, World!\n")
+
+        dir_path = Path(tmpdir).joinpath("test_directory/")
+        os.makedirs(dir_path)
+        path2 = Path(dir_path).joinpath("test_file2.txt")
+
+        with path2.open("w") as fh:
+            fh.write("Hello, Again!\n")
+
+        io.archive_files(target_dir=tmpdir, name="archive", kind=kind)
+
+    if kind == "gztar":
+        kind = "tar.gz"
+
+    os.remove(f"archive.{kind}")
+
+
 class TestQueryYesNo:
     @pytest.mark.parametrize(
         "arg, answer, expected",
@@ -38,26 +61,3 @@ class TestQueryYesNo:
         monkeypatch.setattr("sys.stdin", StringIO("yay"))
         with pytest.raises(EOFError):
             io.query_yes_no("Do you like BumBag?", "yes")
-
-
-@pytest.mark.parametrize("kind", ["zip", "gztar"])
-def test_archive_files(kind):
-    with TemporaryDirectory() as tmpdir:
-        path = Path(tmpdir).joinpath("test_file.txt")
-
-        with path.open("w") as fh:
-            fh.write("Hello, World!\n")
-
-        dir_path = Path(tmpdir).joinpath("test_directory/")
-        os.makedirs(dir_path)
-        path2 = Path(dir_path).joinpath("test_file2.txt")
-
-        with path2.open("w") as fh:
-            fh.write("Hello, Again!\n")
-
-        io.archive_files(target_dir=tmpdir, name="archive", kind=kind)
-
-    if kind == "gztar":
-        kind = "tar.gz"
-
-    os.remove(f"archive.{kind}")
