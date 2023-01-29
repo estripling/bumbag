5cf95de6a647a5c708979acdabdca6aaece566ae
estripling <estripling042@gmail.com>
2023-01-29 20:10:42 +0000

    refactor(cleanup.py): import modules

--- cleanup.py
+++ cleanup.py
@@ -1,23 +1,23 @@
 """A script to clean up Python cache files and directories."""
 
-from pathlib import Path
-from shutil import rmtree
+import pathlib
+import shutil
 
 
 def main():
-    pwd = Path(".")
+    cwd = pathlib.Path(".")
     file_extensions = ["*.py[co]", ".coverage", ".coverage.*"]
     directories = ["__pycache__", ".pytest_cache", ".ipynb_checkpoints"]
 
     for file_extension in file_extensions:
-        for path in pwd.rglob(file_extension):
-            print(f"deleting {path}")
-            path.unlink()
+        for pathlib.path in cwd.rglob(file_extension):
+            print(f"deleting {pathlib.path}")
+            pathlib.path.unlink()
 
     for directory in directories:
-        for path in pwd.rglob(directory):
-            print(f"deleting {path}")
-            rmtree(path.absolute(), ignore_errors=False)
+        for pathlib.path in cwd.rglob(directory):
+            print(f"deleting {pathlib.path}")
+            shutil.rmtree(pathlib.path.absolute(), ignore_errors=False)
 
 
 if __name__ == "__main__":
