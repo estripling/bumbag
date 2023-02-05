6082e4fbaaf048459f7afbfd2cd9c096dc1327e4
estripling <estripling042@gmail.com>
2023-02-05 20:32:16 +0000

    style(src/bumbag/__init__.py): make del statement vc friendly

--- src/bumbag/__init__.py
+++ src/bumbag/__init__.py
@@ -9,4 +9,12 @@ from .random import *
 from .string import *
 from .time import *
 
-del core, io, math, random, string, time, metadata
+del (
+    core,
+    io,
+    math,
+    random,
+    string,
+    time,
+    metadata,
+)
